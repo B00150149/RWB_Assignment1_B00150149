052ae5c7113d21ef54f3a74b650a3aaefe5e30dd
B00150149 <B00150149@mytudublin.ie>
2024-12-01 12:26:22 +0000

    Update route.js

--- krispykremeapp/src/app/api/getProducts/route.js
+++ krispykremeapp/src/app/api/getProducts/route.js
@@ -6,7 +6,7 @@
         const { MongoClient } = require('mongodb');
         // const url = 'mongodb+srv://root:myPassword123@krispykremecluster.2a1di.mongodb.net/?retryWrites=true&w=majority&appName=KrispyKremeCluster'
         const url = process.env.DB_ADDRESS
-try{
+
         const client = new MongoClient(url);
         const dbName = 'app'; // database name
         await client.connect();
@@ -15,10 +15,6 @@ try{
         const collection = db.collection('products'); // collection name
         const findResult = await collection.find({}).toArray();
         console.log('Found documents =>', findResult);
-}catch (error) {
-            console.error('Error fetching products:', error);
-            res.status(500).json({ message: 'Internal Server Error' });
-}
         //==========================================================
         // at the end of the process we need to send something back.
         return Response.json(findResult)
